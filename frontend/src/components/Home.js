c869a89591a4f20f40a7e08e82ae8085a87aa0d5
Jake Formico <jakeformico@gmail.com>
2023-04-13 17:29:48 -0500

    update login page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,67 +1,25 @@
 import React from 'react'
-import {
-  MDBContainer,
-  MDBInput,
-  MDBCheckbox,
-  MDBBtn,
-  MDBIcon
-}
-from 'mdb-react-ui-kit';
-
+import Login from './security/Login';
+import { Link } from "react-router-dom";
 const Home = () => {
-  // return (
-  //     <div className={'main-page'}>
-  //       <div className={'panel-group'}>
-  //         <div className={'panel'}>
-  //           <h1>CSI 5324 Message Board</h1><br /><br />
-            
-  //           <h2>Have an account? Sign in here!</h2>
-
-  //           <h3>Otherwise, register here!</h3>
-  //         </div>
-  //       </div>
-  //     </div>
-  // )
-
   return (
-    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+      <div className={'main-page'}>
+        <div className={'panel-group'}>
+          <div className={'panel'}>
+            <h1>CSI 5324 Message Board</h1><br /><br />
+            
+            <h2>Have an account?</h2>
+            <Link to="./Login">Log In!</Link>
 
-      <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email'/>
-      <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password'/>
-
-      <div className="d-flex justify-content-between mx-3 mb-4">
-        <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-        <a href="!#">Forgot password?</a>
-      </div>
-
-      <MDBBtn className="mb-4">Sign in</MDBBtn>
-
-      <div className="text-center">
-        <p>Not a member? <a href="#!">Register</a></p>
-        <p>or sign up with:</p>
-
-        <div className='d-flex justify-content-between mx-auto' style={{width: '40%'}}>
-          <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-            <MDBIcon fab icon='facebook-f' size="sm"/>
-          </MDBBtn>
-
-          <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-            <MDBIcon fab icon='twitter' size="sm"/>
-          </MDBBtn>
-
-          <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-            <MDBIcon fab icon='google' size="sm"/>
-          </MDBBtn>
-
-          <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-            <MDBIcon fab icon='github' size="sm"/>
-          </MDBBtn>
-
+            <h2>Otherwise, register here</h2>
+            <Link to="./Register">Sign up!</Link>
+           
+          </div>
         </div>
       </div>
+  )
 
-    </MDBContainer>
-  );
+
 
 
 
