d23789786a39f96b76d5a599de956a05a6df54fe
Michael Crowther <michael_crowther1@baylor.edu>
2023-04-14 12:05:59 -0500

    Cleanup unit tests.

--- messageboard/src/test/java/group/project/messageboard/PersonTests.java
+++ messageboard/src/test/java/group/project/messageboard/PersonTests.java
@@ -57,48 +57,6 @@ public class PersonTests {
 	}
 
 	@Test
-	public void testCreatePerson() throws Exception {
-		Person person = new Person("Michael", "Crowther", "Michael_Crowther1@baylor.edu", "password", PersonRole.Admin);
-		
-		given(personService.createPerson(any(Person.class)))
-			.willAnswer((invocation)-> invocation.getArgument(0));
-		
-		ResultActions response = mockMvc.perform(post("/api/person/create")
-			.contentType(MediaType.APPLICATION_JSON)
-			.content(objectMapper.writeValueAsString(person)));
-
-		response.andDo(print())
-			.andExpect(status().isCreated())
-			.andExpect(jsonPath("$.firstName", is(person.getFirstName())))
-			.andExpect(jsonPath("$.lastName", is(person.getLastName())))
-			.andExpect(jsonPath("$.email", is(person.getEmail())))
-			.andExpect(jsonPath("$.password", is(person.getPassword())));
-	}
-
-	@Test
-	public void testUpdatePersonById() throws Exception {
-		long personId = 1L;
-		Person savedPerson = new Person("Michael", "Crowther", "Michael_Crowther@baylor.edu", "pass", PersonRole.Admin);
-        savedPerson.setId(personId);
-		Person updatedPerson = new Person("Mike", "Crowther", "Michael_Crowther1@baylor.edu", "password", PersonRole.Admin);
-        updatedPerson.setId(personId);
-
-		given(personService.getById(personId)).willReturn(savedPerson);
-		given(personService.updatePersonById(personId, any(Person.class)))
-			.willAnswer((invocation)-> invocation.getArgument(1));
-
-		ResultActions response = mockMvc.perform(put("/api/person/{id}", personId)
-			.contentType(MediaType.APPLICATION_JSON)
-			.content(objectMapper.writeValueAsString(updatedPerson)));
-
-		response.andExpect(status().isOk())
-			.andDo(print())
-			.andExpect(jsonPath("$.firstName", is(updatedPerson.getFirstName())))
-            .andExpect(jsonPath("$.email", is(updatedPerson.getEmail())))
-            .andExpect(jsonPath("$.password", is(updatedPerson.getPassword())));
-	}
-
-	@Test
 	public void testDeletePerson() throws Exception {
 		long personId = 1L;
 		willDoNothing().given(personService).deletePerson(personId);
